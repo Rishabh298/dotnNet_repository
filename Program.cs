bcdfce6600a5642ed3ab1edf3923d621f5ac5d24
Rishabh298 <79579236+Rishabh298@users.noreply.github.com>
2024-04-10 10:47:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿class LengthHelper
+{
+    static void Main()
+    {
+        FindLength();
+    }
+
+    static int FindLength()
+    {
+        string userInput;
+        int strLength;
+        do
+        {
+            
+                System.Console.Write("Enter the String: ");
+                userInput = System.Console.ReadLine();
+                strLength = userInput.Length;
+
+            if (userInput.ToLower() != "exit")
+            {
+                System.Console.WriteLine("Length of String is: " + strLength);
+            } else
+            {
+                System.Environment.Exit(0);
+            }
+               
+        } while (userInput.ToLower() != "exit");
+
+        System.Console.ReadKey();
+        return strLength;
+
+    }
+}
